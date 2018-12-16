69e6a9f7e7d39594283fd7246699fbda4a17e5da
Serges Pemba <42515690+spemba@users.noreply.github.com>
2018-12-16 23:46:37 +0100

    Add files via upload

---
+++ search_server.py
@@ -0,0 +1,38 @@
+from flask import Flask, request
+app = Flask(__name__, static_folder='static/search/', static_url_path='')
+import json
+import sys
+
+import coffeescript
+
+from searcher import Searcher
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+@app.route('/search-api', methods=['POST'])
+def search_api():
+    return app.searcher.search(request.get_json())
+
+def compile_assets():
+    static = app.static_folder
+
+    with open("{}/javascript/index.js".format(static), 'w') as f:
+        infile = "{}/coffeescript/index.coffee".format(static)
+        js = coffeescript.compile_file(infile)
+        f.write(js)
+
+def server(config):
+    print('Compiling assets...')
+    compile_assets()
+
+    app.searcher = Searcher(config)
+    return app
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: {} config.toml".format(sys.argv[0]))
+        sys.exit(1)
+
+    server(sys.argv[1]).run(debug=True)
